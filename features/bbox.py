cbbc8784179b6661fc9b5311882ea5b45d23d3bc
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-07 16:14:06 +0000

    Equivalent to ml_cropbg
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@10 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ features/bbox.py
@@ -0,0 +1,53 @@
+from load import *
+from threshold import *
+from numpy import *
+__all__ = ['bbox', 'croptobbox', 'expandto']
+def bbox(img):
+    """
+    min1,max1,min2,max2 = bbox(img)
+
+    Calculate the bounding box of image img.
+    """
+    min1=0
+    min2=0
+    max1,max2=img.shape
+    while min1 < max1 and all(img[min1,:]==0):
+        min1 += 1
+
+    while max1 > min1 and all(img[max1-1,:]==0):
+        max1 -= 1
+
+    while min2 < max2 and all(img[:,min2]==0):
+        min2 += 1
+
+    while max2 > min2 and all(img[:,max2-1]==0):
+        max2 -= 1
+
+    return min1,max1,min2,max2
+
+def croptobbox(img):
+    """
+    Returns a cropped version of img.
+    """
+    
+    min1,max1,min2,max2 = bbox(img)
+    return img[min1:max1,min2:max2]
+
+def expandto(img,S1,S2):
+    """
+    Returns a zero padded version of img
+    so that it is of size (S1,S2).
+
+    Img will be on the top left of the result.
+    """
+
+    result=zeros((S1,S2),img.dtype)
+    M,N=img.shape
+    assert M <= S1
+    assert N <= S2
+    st1=(S1-M)//2
+    st2=(S2-N)//2
+    result[st1:(st1+M),st2:(st2+N)]=img
+    return result
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
