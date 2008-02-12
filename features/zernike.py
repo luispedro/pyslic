8beedef012bf9a1752e91e67e9377da97fc92942
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-12 02:33:08 +0000

    Fix bugs by using the MurphyLab's as a reference
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@44 7947ee52-e672-4f94-911f-8123aba2f436

--- features/zernike.py
+++ features/zernike.py
@@ -21,6 +21,7 @@
 
 # 19 Dec 98 - M.V. Boland
 
+from __future__ import division
 from math import *
 from numpy import *
 from scipy.ndimage import *
@@ -42,7 +43,7 @@ def Znl(n,l,X,Y,P):
     v = 0.+0.j
     for x,y,p in zip(X,Y,P):
         Vnl = 0.
-        for m in xrange( (n-l)//2 ):
+        for m in xrange( (n-l)//2 + 1 ):
               Vnl += (-1.)**m * _factorial(n-m) /  \
             ( _factorial(m) * _factorial((n - 2*m + l) // 2) * _factorial((n - 2*m - l) // 2) ) * \
             ( sqrt(x*x + y*y)**(n - 2*m) * _polar(1.0, l*atan2(y,x)) )
@@ -54,7 +55,7 @@ def Znl(n,l,X,Y,P):
 
 def zernike(img,D,radius):
     """
-     ZNAMES, ZVALUES = zernike(img,D,radius) Zernike moments through degree D 
+     ZVALUES = zernike(img,D,radius) Zernike moments through degree D 
      ML_ZERNIKE(I,D,R),
      Returns a vector of Zernike moments through degree D for the
      image I, and the names of those moments in cell array znames. 
@@ -70,25 +71,28 @@ def zernike(img,D,radius):
     zvalues = []
 
 # Find all non-zero pixel coordinates and values
-    Y,X = where(img)
+    X,Y = where(img > 0)
+    P=img[X,Y].ravel()
 
 # Normalize the coordinates to the center of mass and normalize
-#  pixel distances using the maximum radius argument (radiu)
+#  pixel distances using the maximum radius argument (radius)
     cofx,cofy = center_of_mass(img)
     Xn = double(X-cofx)/radius
     Yn = double(Y-cofy)/radius
+    Xn=Xn.ravel()
+    Yn=Yn.ravel()
+
 
 # Find all pixels of distance <= 1.0 to center
     k = (sqrt(Xn**2 + Yn**2) <= 1.)
-    frac_center = array(img[X[k],Y[k]],double)/(img>0).sum()
+    frac_center = array(P[k],double)/img.sum()
 
     for n in xrange(D+1):
         for l in xrange(n+1):
             if (n-l)%2 == 0:
                 znames.append('Z_#i,%s%s' % (n, l))
-                z= Znl(n,l, Xn[k], Yn[k], frac_center)
-                zvalues.append(z.real)
-                zvalues.append(z.imag)
+                z= Znl(n,l, Xn[k], Yn[k], frac_center.ravel())
+                zvalues.append(abs(z))
     return zvalues
 
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
