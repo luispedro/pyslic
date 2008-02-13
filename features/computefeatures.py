dc391f1f02a4ee1de96244952f15e6d52f25036c
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-13 19:35:54 +0000

    Compute features by feature set name
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@48 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ features/computefeatures.py
@@ -0,0 +1,54 @@
+from sys import path
+path.append('../')
+from string import upper
+from image import Image
+from preprocess import preprocess
+from numpy import *
+
+from edgefeatures import edgefeatures
+from texture import haralickfeatures
+from imgskelfeats import imgskelfeatures
+from noffeatures import noffeatures
+from imgfeatures import imgfeatures
+from hullfeatures import hullfeatures
+from zernike import zernike
+
+__all__ = ['computefeatures']
+
+def _featsfor(featset):
+    featset=upper(featset)
+    if featset == 'SLF13' or featset == 'SLF7DNA':
+        return ['skl','nof','img','hul','zer','har','edg']
+
+def computefeatures(img,featsets):
+    if type(featsets) == str:
+        featsets = _featsfor(featsets)
+    preprocess(img,1,{})
+    features=array([])
+    procprotein=img.channeldata[Image.procprotein_channel]
+    resprotein=img.channeldata[Image.residualprotein_channel]
+    procdna=img.channeldata.get(Image.procdna_channel)
+    for F in featsets:
+        if F == 'edg':
+            feats=edgefeatures(procprotein)
+        elif F == 'har':
+            feats=haralickfeatures(procprotein)
+            feats=feats.mean(0)
+        elif F == 'hul':
+            feats=hullfeatures(procprotein)
+        elif F == 'img':
+            feats=imgfeatures(procprotein,procdna)
+        elif F == 'mor':
+            feats=morphologicalfeatures(procprotein)
+        elif F == 'nof':
+            feats=noffeatures(procprotein,resprotein)
+        elif F == 'skl':
+            feats=imgskelfeatures(procprotein)
+        elif F == 'zer':
+            feats=zernike(procprotein,12,34.5)
+        else:
+            raise Exception('Unkown feature set: %s' % F)
+        features = r_[features,feats]
+    return features
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
