7827cca9e5f1195435d7028214f02a10096c6f80
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-13 19:45:14 +0000

    implement a majority filter
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@51 7947ee52-e672-4f94-911f-8123aba2f436

--- preprocess/basics.py
+++ preprocess/basics.py
@@ -1,4 +1,7 @@
-from scipy.ndimage import histogram
+from scipy.ndimage import histogram, convolve
+from numpy import asarray, ones, uint32
+
+__all__ = ['fullhistogram','majority_filter']
 def fullhistogram(img):
     """
     Return a histogram containing all the values in the image
@@ -7,6 +10,16 @@ def fullhistogram(img):
     maxt=img.max()
     return histogram(img,mint,maxt,maxt-mint+1)
 
+def majority_filter(bwimg, N = 3):
+    """
+    Implement a majority filter of size 0.
 
+    bwimg is interpreted as a binary image (non-zero pixels correspond to one/True).
+    """
+    assert N < 2 ** 16
+    bwimg=asarray(bwimg > 0,uint32)
+    F=ones((N,N))
+    bwimg=convolve(bwimg,F)
+    return bwimg > (N**2/2)
 
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
