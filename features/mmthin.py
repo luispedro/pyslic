abba3b7b38d68e9a247cf1b3abfc1b2b03eeeee3
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-17 23:33:45 +0000

    Implement hit_or_miss by using ml_hitmiss The nd implementation was, for some reason, not satisfactorv
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@70 7947ee52-e672-4f94-911f-8123aba2f436

--- features/mmthin.py
+++ features/mmthin.py
@@ -21,6 +21,7 @@
 
 from numpy import *
 from scipy.ndimage import binary_hit_or_miss
+from scipy.misc.pilutil import imshow
 
 __all__ = ['mmthin']
 def mmthin(binimg):
@@ -39,30 +40,99 @@ def mmthin(binimg):
     num_elem = abs(360//degrees)
 
     struct_elem = []
-    struct_elem.append([[0,0,0],[2,1,2],[1,1,1]])
-    struct_elem.append([[2,0,0],[1,1,0],[1,1,2]])
-    struct_elem.append([[1,2,0],[1,1,0],[1,2,0]])
-    struct_elem.append([[1,1,2],[1,1,0],[2,0,0]])
-    struct_elem.append([[1,1,1],[2,1,2],[0,0,0]])
-    struct_elem.append([[2,1,1],[0,1,1],[0,0,2]])
-    struct_elem.append([[0,2,1],[0,1,1],[0,2,1]])
-    struct_elem.append([[0,0,2],[0,1,1],[2,1,1]])
+    struct_elem.append([
+            [0,0,0],
+            [2,1,2],
+            [1,1,1]])
+    struct_elem.append([
+            [2,0,0],
+            [1,1,0],
+            [1,1,2]])
+    struct_elem.append([
+            [1,2,0],
+            [1,1,0],
+            [1,2,0]])
+    struct_elem.append([
+            [1,1,2],
+            [1,1,0],
+            [2,0,0]])
+    struct_elem.append([
+            [1,1,1],
+            [2,1,2],
+            [0,0,0]])
+    struct_elem.append([
+            [2,1,1],
+            [0,1,1],
+            [0,0,2]])
+    struct_elem.append([
+            [0,2,1],
+            [0,1,1],
+            [0,2,1]])
+    struct_elem.append([
+            [0,0,2],
+            [0,1,1],
+            [2,1,1]])
 
+    struct_elem=[array(E) for E in struct_elem]
     r,c=binimg.shape
-    total_oper = 0;
     acnum_elem = 0;
+    total_op=0
 
     while True:
-        acnum_elem +=  1;
-        image_exp = zeros((r+4, c+4))
-        image_exp[2:r+2, 2:c+2] = binimg
-        newimg=binary_hit_or_miss(image_exp,struct_elem[acnum_elem])
-        binimg -= newimg[2:r+2,2:c+2]
+        image_exp = zeros((r+2, c+2))
+        image_exp[1:r+1, 1:c+1] = binimg
+        newimg=hitmiss(image_exp,struct_elem[acnum_elem])
+        binimg -= newimg[1:r+1,1:c+1]
+        total_op += newimg.sum()
 
-        if acnum_elem == num_elem - 1:
-            acnum_elem = 0
-            if newimg.sum() == 0:
+        acnum_elem +=  1;
+        if acnum_elem == num_elem:
+            if total_op == 0:
                 break
+            acnum_elem = 0
+            total_op = 0
     return binimg
 
+def hitmiss(binimg,struct_elem):
+    '''
+    Implementation of hit-or-miss operation
+    '''
+# Adapted from ml_mmhitmiss
+
+    r,c=binimg.shape
+    changed_image = zeros((r,c))
+    try:
+        from scipy import weave
+        from scipy.weave import converters
+        code = '''
+#line 105 "mmthin.py"
+        for (int y = 0; y != r-2 ; ++y) {
+            for (int x = 0; x != c-2; ++x) {
+                int hits = 0;
+                for (int w = 0; w != 3; ++w) {
+                    for (int z = 0; z != 3; ++z) {
+                        if (struct_elem(w,z) == binimg(y+w,x+z) || struct_elem(w,z) == 2) ++hits;
+                    }
+                }
+                if (hits == 9) changed_image(y+1,x+1) = 1;
+            }
+        }
+        '''
+        weave.inline(code,
+            ['r','c','binimg','changed_image','struct_elem'],
+            type_converters=converters.blitz
+            )
+    except Exception, e:
+        print 'Weave failed. Resorting to (slow) python code'
+        changed_image = zeros((r-2,c-2))
+        for y in xrange(r-2):
+            for x in xrange(c-2):
+                hits = 0
+                for w in xrange(3):
+                    for z in xrange(3):
+                        if struct_elem[w,z] == binimg[y+w,x+z] or struct_elem[w,z] == 2:
+                            hits += 1
+                if hits == 9:
+                    changed_image[y+1,x+1]=1
+    return changed_image
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
