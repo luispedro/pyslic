d392b5c8a5a72f708831e26b1867ac89af89d839
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-17 22:29:13 +0000

    Implement N-Fold Cross Validation
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@69 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ classify/nfoldcrossvalidation.py
@@ -0,0 +1,33 @@
+from __future__ import division
+from numpy import *
+from classify import *
+
+__all__=['nfoldcrossvalidation']
+def nfoldcrossvalidation(features,labels,nfolds=10,train=learnclassifier,test=applyclassifier):
+    classcounts={}
+    for L in labels:
+        classcounts[L] = classcounts.get(L,0) + 1
+    
+    nclasses=len(classcounts)
+    cmatrix=zeros((nclasses,nclasses))
+    for fold in xrange(nfolds):
+        testingset=zeros(len(labels),bool)
+
+        for L,C in classcounts.items():
+            idxs=where(labels==L)[0]
+            N=len(idxs)
+            perfold=N/nfolds
+            start=floor(perfold*fold)
+            end=floor(perfold*(fold+1))
+            idxs=idxs[start:end]
+            testingset[idxs]=True
+        trainingset= ~testingset
+        
+        model=train(features[trainingset],labels[trainingset])
+        prediction=test(features[testingset],model)
+        for p, r in zip(prediction,labels[testingset]):
+            cmatrix[r,p] += 1
+
+    return cmatrix
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
