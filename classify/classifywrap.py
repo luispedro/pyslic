7712744a163163add1b2531833cbd7ce82fe8c8d
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-17 21:59:16 +0000

    Make it easy to wrap a feature transformation before classifier
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@66 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ classify/classifywrap.py
@@ -0,0 +1,16 @@
+__all__ = ['pretransformclassifier']
+class pretransformclassifier(object):
+    def __init__(self,transformer,classifier):
+        self.transformer=transformer
+        self.classifier=classifier
+
+    def train(self,features,labels):
+        self.transformer.train(features,labels)
+        features=self.transformer.apply(features)
+        self.classifier.train(features,labels)
+    
+    def apply(self,features):
+        features=self.transformer.apply(features)
+        return self.classifier.apply(features,labels)
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
--- classify/normalise.py
+++ classify/normalise.py
@@ -2,14 +2,27 @@ def normalise(features):
     """
     features = normalise(features)
 
-    Returns a copy of features which has been normalised to zscores and
-    with constant features removed    
+    Returns a copy of features which has been normalised to zscores 
     """
-    sigma=features.std(0)
-    features=features[:,sigma != 0]
     mu=features.mean(0)
     features-= mu
+    sigma=features.std(0)
+    sigma[sigma == 0] = 1
     features /= sigma[sigma != 0]
     return features
+
+class normaliser(object):
+    __slots__=['mu','sigma']
+    def __init__(self,features=None):
+        if features:
+            self.train(features)
+
+    def train(self,features,labels):
+        self.mu=features.mean(0)
+        self.sigma=features.std(0)
+        self.sigma[self.sigma == 0.]=1
+
+    def apply(self,features):
+        return (features - self.mu)/self.sigma
 
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
