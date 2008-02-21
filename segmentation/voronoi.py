c6a0892633e695ae2f093685d67540bc1ee24e10
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-21 22:44:16 +0000

    Implement generalised voronoi transform
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@97 7947ee52-e672-4f94-911f-8123aba2f436

--- segmentation/voronoi.py
+++ segmentation/voronoi.py
@@ -1,6 +1,9 @@
 from __future__ import division
 from numpy import *
-__all__ = ['voronoi']
+from scipy.ndimage import distance_transform_edt,label, median_filter
+from ..preprocess.thresholding import otsu
+
+__all__ = ['voronoi','gvoronoi']
 def voronoi(img,centers,distance='euclidean'):
     '''
     labeled = voronoi(img,centers,distance)
@@ -20,7 +23,7 @@ def voronoi(img,centers,distance='euclidean'):
         dx=x-cx
         return abs(dy)+abs(dx)
     dist=seuclidean
-    if distance == 'manhatan':
+    if distance == 'manhatan' or distance == 'cityblock' or distance == 'city_block':
         dist=manhatan
     for y in xrange(r):
         for x in xrange(c):
@@ -32,4 +35,20 @@ def voronoi(img,centers,distance='euclidean'):
                     besti=i+1
             labels[y,x]=besti
     return labels
+
+def gvoronoi(dnaimg,labelednuclei=None,distance='euclidean'):
+    """
+    labeled = gvoronoi(dnaimg,labelednuclei,distance)
+
+    Generalised Voronoi Transform
+    """
+    if labelednuclei is None:
+        dnaimg=median_filter(dnaimg,4)
+        T=otsu(dnaimg)
+        labelednuclei,_=label(dnaimg > T)
+    if distance == 'euclidean':
+        L1,L2=distance_transform_edt(labelednuclei == 0, return_distances=False,return_indices=True)
+    else:
+        raise Exception('gvoronoi: Distance "%s" not implemented' % distance)
+    return labelednuclei[L1,L2]
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
