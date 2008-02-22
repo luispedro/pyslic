8686b36691e671a7fc712e7ca686891d37cb1a4c
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-02-22 22:12:50 +0000

    Documentation Use absolute paths
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@110 7947ee52-e672-4f94-911f-8123aba2f436

--- readimages/ksr.py
+++ readimages/ksr.py
@@ -7,6 +7,12 @@ from ..image import Image
 __all__ = ['read_ksr_dir']
 
 def read_ksr_dir(dir):
+    '''
+    images = read_ksr_dir(dirname)
+
+    Read all the files in dirname and return them as a dictionary:
+        (WellName, FieldNr) -> Image
+    '''
     Files=os.listdir(dir)
     pat=re.compile('KSR_.*t([0-9]+)([A-H][0-9]{1,2})f([0-9]+)d([0-9])\.(tif|TIF)')
     channelcode = { 1 : Image.dna_channel , 2 : Image.protein_channel, 3 : Image.autofluorescence_channel } 
@@ -17,12 +23,16 @@ def read_ksr_dir(dir):
             print "Don't know how to process", f
             continue
         T,Well,Field,Channel,_=m.groups()
+        T=int(T)
         Field=int(Field)
+        if T != 1:
+            print "Don't know how to handle more than one time point, ignoring"
+            continue
         img=images.get((Well,Field),None)
         if img is None:
             img = Image()
             images[(Well,Field)]=img
-        imgs.channels[channelcode[int(Channel)]]=os.path.join(dir,f)
+        img.channels[channelcode[int(Channel)]]=os.path.abspath(os.path.join(dir,f))
     return images
 
 
