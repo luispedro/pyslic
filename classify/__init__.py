2d143ad11c65ae2d9fb9f3bd62ab542b16e4908d
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-03-03 20:55:58 +0000

    Make this into a module
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@131 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ classify/__init__.py
@@ -0,0 +1,5 @@
+from accuracy import accuracy, waccuracy, rowsto1
+from classify import *
+from nfoldcrossvalidation import nfoldcrossvalidation
+import normalise
+from normalise import zscore, normaliser, chkfinite
