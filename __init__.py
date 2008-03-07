60656f4906e4d42bf69a597b2dbf18339aaf9b0c
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-03-07 03:53:58 +0000

    expose read_ksr_dir
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@132 7947ee52-e672-4f94-911f-8123aba2f436

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@ from .features import computefeatures
 import image
 from image import Image
 from .preprocess import preprocessimage, bgsub
+import readimages
 __all__ = ['image','Image','preprocess','bgsub','computefeatures']
---
+++ readimages/__init__.py
@@ -0,0 +1,3 @@
+from .ksr import read_ksr_dir
+
+__all__ = ['read_ksr_dir']
