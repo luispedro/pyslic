5b570e66dfcb076f479b280af11b7a49d50595f7
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-04-08 17:20:08 +0000

    Allow user to specify loadfunction (better than post load actions) Implement pickling interface
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@199 7947ee52-e672-4f94-911f-8123aba2f436

--- image/image.py
+++ image/image.py
@@ -3,7 +3,7 @@ from numpy import *
 
 __all__ = ['Image', 'setshowimage']
 
-def _open_file(fname):
+def _open_file_bw(fname):
     """
     Force a file to be B&W
     """
@@ -22,6 +22,7 @@ def setshowimage(f):
     global _showimage
     _showimage = f
 
+
 class Image(object):
     """
     class Image(object)
@@ -37,16 +38,44 @@ class Image(object):
     procprotein_channel='procprotein'
     residualprotein_channel='resprotein'
 
-    __slots__ = ['label','scale','regions','channels','channeldata','loaded','__post_load_actions']
+    __slots__ = ['label','scale','regions','channels','channeldata','loaded','__loadfunction']
 
     def __init__(self):
+        '''
+        Create an Image.
+
+        Fill the elements in channels to meaningful values.
+        '''
         self.label=''
         self.scale = None
-        self.regions=None
         self.channels={}
-        self.channeldata={}
+        self.__loadfunction = _open_file_bw
+
         self.loaded=False
-        self.__post_load_actions = []
+        self.regions=None
+        self.channeldata={}
+
+    def __getstate__(self):
+        return (self.label,self.scale,self.channels,self.loaded,self.__loadfunction)
+
+    def __setstate__(self,S):
+        self.label,self.scale,self.channels,self.loaded,self.__loadfunction = S
+
+        self.loaded = False
+        self.regions = None
+        self.channeldata=[]
+
+    def set_load_function(self,f):
+        '''
+        Sets the file loading function whose signature should be
+            string -> numpy array
+
+        The default function does imread() followed by conversion to B&W
+            by taking the mean pixel value of the available channels
+            (this assumes that the image really is B&W, it was just saved 
+            as a colour image)
+        '''
+        self.__loadfunction = f
 
     def lazy_load(self):
         '''
@@ -54,14 +83,6 @@ class Image(object):
         '''
         if not self.loaded:
             self.load()
-
-    def append_post_load(self,action):
-        '''
-        Register a post load action
-        
-        action must be callable with a single parameter, the image.
-        '''
-        self.__post_load_actions.append(action)
 
     def load(self):
         '''
@@ -71,15 +92,13 @@ class Image(object):
         '''
         for k,v in self.channels.items():
             if k != self.crop_channel: # Crop is handled like a region
-                self.channeldata[k]=_open_file(v)
+                self.channeldata[k]=self.__loadfunction(v)
         if self.crop_channel in self.channels:
             # These files often need to be fixed:
-            self.regions = _open_file(self.channels[self.crop_channel])
+            self.regions = self.__loadfunction(self.channels[self.crop_channel])
             if self.regions.max() == 255:
                 self.regions[self.regions == 255] = 1
         self.loaded = True
-        for action in self.__post_load_actions:
-            action(self)
 
     def unload(self):
         '''
@@ -90,26 +109,26 @@ class Image(object):
         self.loaded=False
 
 
-    def composite(self):
+    def composite(self,processed = False):
         '''
-        composite = img.composite()
+        composite = img.composite(processed = False)
 
         Returns a multi colour image containing the different channels
         '''
         self.lazy_load()
         X,Y=self.channeldata[self.protein_channel].shape
         composite=zeros((X,Y,3))
-        composite[:,:,1]=self.channeldata[self.protein_channel]
+        composite[:,:,1]=self.channeldata[(self.procprotein_channel if processed else self.protein_channel)]
         if self.dna_channel in self.channeldata:
-            composite[:,:,0]=self.channeldata[self.dna_channel]
+            composite[:,:,0]=self.channeldata[(self.procdna_channel if processed else self.dna_channel)]
         return composite
 
-    def show(self):
+    def show(self,processed = False):
         '''
         Shows the image composite
 
         See composite.
         '''
-        _showimage(self.composite())
+        _showimage(self.composite(processed))
 
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
