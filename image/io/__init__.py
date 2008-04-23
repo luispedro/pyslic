7441bffe3ca773acdad0506dc96b59849675863f
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-04-23 18:15:17 +0000

    Make read_ksr_dir return a list of images (use img.label & img.id to pass out information about where the images came from) Implement KSR dir detection
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@226 7947ee52-e672-4f94-911f-8123aba2f436

--- image/io/__init__.py
+++ image/io/__init__.py
@@ -1,3 +1,3 @@
 from readtjz import readtjz_recursive
-from ksr import read_ksr_dir
+from ksr import read_ksr_dir, detect_ksr_dir
 from ic100 import detect_ic100dir, read_ic100dir
--- image/io/ksr.py
+++ image/io/ksr.py
@@ -1,10 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008  Murphy Lab
+# Carnegie Mellon University
+# 
+# Written by Luís Pedro Coelho <lpc@cmu.edu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# For additional information visit http://murphylab.web.cmu.edu or
+# send email to murphy@cmu.edu
+
+from __future__ import division
 import re
 import os.path
 import os
 import sys
 from ..image import Image
 
-__all__ = ['read_ksr_dir']
+__all__ = ['read_ksr_dir','detect_ksr_dir']
+_ksrpat=re.compile('KSR_.*t([0-9]+)([A-H][0-9]{1,2})f([0-9]+)d([0-9])\.(tif|TIF)')
+
+def detect_ksr_dir(dir):
+    '''
+    is_ksr_dir = detect_ksr_dir(dir)
+
+    Returns true if the directory seems to contain ksr files
+    '''
+    f=os.listdir(dir)[0]
+    return _ksrpat.match(f)
 
 def read_ksr_dir(dir):
     '''
@@ -14,11 +49,10 @@ def read_ksr_dir(dir):
         (WellName, FieldNr) -> Image
     '''
     Files=os.listdir(dir)
-    pat=re.compile('KSR_.*t([0-9]+)([A-H][0-9]{1,2})f([0-9]+)d([0-9])\.(tif|TIF)')
     channelcode = { 1 : Image.dna_channel , 2 : Image.protein_channel, 3 : Image.autofluorescence_channel } 
     images={}
     for f in Files:
-        m=pat.match(f)
+        m=_ksrpat.match(f)
         if not m:
             print "Don't know how to process", f
             continue
@@ -31,9 +65,11 @@ def read_ksr_dir(dir):
         img=images.get((Well,Field),None)
         if img is None:
             img = Image()
+            img.label = Well
+            img.id = (Well,Field)
             images[(Well,Field)]=img
         img.channels[channelcode[int(Channel)]]=os.path.abspath(os.path.join(dir,f))
-    return images
+    return list(images.values())
 
 
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
