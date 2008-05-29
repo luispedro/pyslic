b7f3cda8663a20efc9cf199c9259b63bee621616
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-05-29 15:12:06 +0000

    We don't need all these libraries
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@256 7947ee52-e672-4f94-911f-8123aba2f436

--- image/io/setup.py
+++ image/io/setup.py
@@ -1,6 +1,6 @@
 from numpy.distutils.core import setup, Extension
 
-magick_libraries= ['Magick++','Wand','Magick','lcms','tiff','freetype','jasper','jpeg','png','Xext','Xt','SM','ICE','X11','bz2','xml2','z','pthread','pthread']
+magick_libraries= ['Magick++','Wand','Magick']
 readimg = Extension('readimg', sources = ['readimg.cpp'], libraries=magick_libraries) 
 
 setup (name = 'readimg',
