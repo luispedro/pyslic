64724deaf4acad5ea2cdec812e18dfe280e12aad
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-05-30 23:15:07 +0000

    Export more
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@265 7947ee52-e672-4f94-911f-8123aba2f436

--- __init__.py
+++ __init__.py
@@ -2,8 +2,13 @@ from .features import computefeatures
 import image
 from image import Image
 import preprocess
+import classify
+import utils
 from .preprocess import preprocessimage, bgsub
 __all__ = ['image',
         'Image',
         'preprocess',
-        'computefeatures']
+        'computefeatures',
+        'classify',
+        'utils',
+        ]
