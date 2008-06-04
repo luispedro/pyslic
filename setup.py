64831bd593732d6d9986c7a74cdf62fff275ea12
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-06-04 14:19:14 +0000

    A more standard setup.py file
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@271 7947ee52-e672-4f94-911f-8123aba2f436

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages, Extension
+
+convexhull = Extension('features/_convexhull', sources = ['features/convexhull.cpp'])
+
+magick_libraries= ['Magick++','Wand','Magick'] # This is gotten from magick++-config --libs
+readimg = Extension('image.io.readimg', sources = ['image/io/readimg.cpp'], libraries=magick_libraries) 
+
+setup(name='PySLIC',
+      version='0.3',
+      description='Subcellular Location Image Classifier',
+      author='Murphy Lab',
+      author_email='murphy@mcu.edu',
+      url='http://murphylab.cbi.cmu.edu/',
+      packages=find_packages(),
+      ext_modules = [convexhull,readimg],
+      )
+
+
