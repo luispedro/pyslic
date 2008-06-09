680af2a8923ea0c2449ce24f155414e1f0079847
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-06-09 18:57:50 +0000

    Change the signature: return the positives array Factor out the region filtering code Write a new filter to remove border regions
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@279 7947ee52-e672-4f94-911f-8123aba2f436

--- segmentation/filters.py
+++ segmentation/filters.py
@@ -1,55 +1,95 @@
 from __future__ import division
+import numpy
 from numpy import *
-from scipy.ndimage import label
+from scipy import ndimage
 from ..imageprocessing.bwperim import bwperim
 from ..imageprocessing.bbox import croptobbox
+from ..imageprocessing.convexhull import convexhull
 import warnings
 
-__all__ = ['prefilter']
-def prefilter(dnamasks,scale,minarea,maxarea,minroundness):
+__all__ = ['dna_size_shape','border_regions','filter_labeled']
+def dna_size_shape(dnamasks,scale,minarea,maxarea,minroundness):
     '''
-    labeled, N = prefilter(dnamasks,scale,minarea,maxarea,minroundness)
+    positives = dna_size_shape(dnamasks,scale,minarea,maxarea,minroundness)
 
-    Performs a filtering version of label(), where objects are kept
-        only if they 
+    Only accepts DNA objects that fulfill the following criterion:
+            * are greater than minarea
+            * are smaller than maxarea
+            * are rounder than minroundness
     '''
 
-    labeled,N=label(dnamasks)
+    labeled,nr_objs=ndimage.label(dnamasks)
 
-    positives=zeros(N)
+    positives=zeros(nr_objs+1,numpy.bool)
     minarea /= scale
     maxarea /= scale
-    for obj in xrange(1,N+1):
+    for obj in xrange(1,nr_objs+1):
         objimg = croptobbox(labeled == obj)
         area=objimg.sum()
-        print area, maxarea, minarea
         if area > maxarea or area < minarea:
             continue
-        perim=bwperim(objimg).sum()
-        roundness=perim**2/(4*pi*area)
-        print roundness
+        hull=convexhull(objimg)
+        hullArea=hull.sum()
+        hullPerim=bwperim(hull).sum()
+        roundness=hullPerim**2/(4*pi*hullArea)
         if roundness < minroundness:
             continue
-        positives[obj-1]=1
+        positives[obj]=1
+    return positives
+
+def border_regions(mask_or_labeled):
+    '''
+    positives = border_regions(mask_or_labeled)
+
+    Removes anything that touches the border.
+    '''
+    nr_objs = mask_or_labeled.max()
+    if mask_or_labeled.max() == 1:
+        labeled,nr_objs=ndimage.label(mask_or_labeled)
+    else:
+        labeled = mask_or_labeled
+    positives = numpy.ones(nr_objs+1,numpy.bool)
+    r,c=labeled.shape
+
+    negs=unique(labeled[0])
+    positives[negs]=False
+
+    negs=unique(labeled[-1])
+    positives[negs]=False
+
+    negs=unique(labeled[:,0])
+    positives[negs]=False
+
+    negs=unique(labeled[:,-1])
+    positives[negs]=False
+    return positives
+
+def filter_labeled(labeled,positives):
+    '''
+    labeled, N = filter_labeled(labeled,positives)
+
+    Performs a filtering version of label(), where object OBJ is kept
+        only if positives[OBJ]
+    '''
+    new_label=cumsum(positives)
+    new_label[~positives]=0
+    D1,D2=labeled.shape
     try:
         from scipy import weave
         from scipy.weave import converters
-        D1,D2=labeled.shape
         code = '''
         for (int y = 0; y != D1; ++y) {
             for (int x = 0; x != D2; ++x) {
-                if (labeled(y,x)) {
-                    if (!positives(labeled(y,x)-1)) labeled(y,x) = 0;
-                }
+                labeled(y,x) = new_label(labeled(y,x));
             }  
         }'''
         weave.inline(code,
-            ['labeled','positives','D1','D2'],
+            ['labeled','new_label','D1','D2'],
             type_converters=converters.blitz)
     except Exception, e:
         warnings.warn('scipy.weave failed. Resorting to (slow) Python code (Error: %s)' % e)
-        for obj in xrange(N):
-            if not positives[obj]:
-                labeled[labeled == (obj+1)]=0
+        for y in xrange(D1):
+            for x in xrange(D2):
+                labeled[y,x]=new_label[labeled[y,x]]
     return labeled,int(positives.sum())
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
