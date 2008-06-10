0927216f4b306173bd47cbc83e78d68a80c496cd
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-06-10 03:15:09 +0000

    readimg needs to be built by numpy.distutils
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@284 7947ee52-e672-4f94-911f-8123aba2f436

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup, find_packages, Extension
+import setuptools
+import numpy.distutils.core as numpyutils
 
-convexhull = Extension('imageprocessing/_convexhull', sources = ['imageprocessing/convexhull.cpp'])
+convexhull = numpyutils.Extension('imageprocessing/_convexhull', sources = ['imageprocessing/convexhull.cpp'])
 
 magick_libraries= ['Magick++','Wand','Magick'] # This is gotten from magick++-config --libs
-readimg = Extension('image.io.readimg', sources = ['image/io/readimg.cpp'], libraries=magick_libraries) 
+readimg = numpyutils.Extension('image.io.readimg', sources = ['image/io/readimg.cpp'], libraries=magick_libraries) 
 
-setup(name='PySLIC',
+numpyutils.setup(name='PySLIC',
       version='0.3',
       description='Subcellular Location Image Classifier',
       author='Murphy Lab',
       author_email='murphy@mcu.edu',
       url='http://murphylab.cbi.cmu.edu/',
-      packages=find_packages(),
+      packages=setuptools.find_packages(),
       ext_modules = [convexhull,readimg],
       )
 
