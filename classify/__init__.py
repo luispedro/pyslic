2bc8779f4853a7cc362a7170a0e1387ca61efe44
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-06-13 21:52:43 +0000

    typo
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@295 7947ee52-e672-4f94-911f-8123aba2f436

--- classify/__init__.py
+++ classify/__init__.py
@@ -1,6 +1,6 @@
 from accuracy import accuracy, waccuracy, rowsto1
 from classify import *
-from libsvmClassifier import libsvmClassifier
+from libsvmclassifier import libsvmClassifier
 from classifywrap import concattransformers, pretransformclassifier
 from nfoldcrossvalidation import nfoldcrossvalidation
 import normalise
