783572c211acf5d23440207d08c0523a3a9159b0
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-06-13 21:24:43 +0000

    Export more
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@293 7947ee52-e672-4f94-911f-8123aba2f436

--- classify/__init__.py
+++ classify/__init__.py
@@ -1,5 +1,7 @@
 from accuracy import accuracy, waccuracy, rowsto1
 from classify import *
+from libsvmClassifier import libsvmClassifier
+from classifywrap import concattransformers, pretransformclassifier
 from nfoldcrossvalidation import nfoldcrossvalidation
 import normalise
 from normalise import *
