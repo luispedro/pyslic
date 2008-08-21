337ae81862025bff5c24ed162293d1a4145c3c5c
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-08-21 03:10:51 +0000

    Add Copyright statement Don't fail on import, but on usage
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@313 7947ee52-e672-4f94-911f-8123aba2f436

--- classify/libsvmclassifier.py
+++ classify/libsvmclassifier.py
@@ -1,23 +1,56 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008  Murphy Lab
+# Carnegie Mellon University
+# 
+# Written by Luís Pedro Coelho <lpc@cmu.edu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# For additional information visit http://murphylab.web.cmu.edu or
+# send email to murphy@cmu.edu
+
 from __future__ import division
 from numpy import array, asanyarray
 from classifier import classifier, classification_result
+_svm = None
 try:
-    from svm import *
+    import svm
+    _svm = svm
 except:
-    from libsvm.svm import *
+    pass
+try:
+    from libsvm import svm
+    _svm = svm
+except:
+    pass
 
 __ALL__=['libsvmClassifier']
 class libsvmClassifier(classifier):
     def __init__(self):
         classifier.__init__(self)
-        self.param = svm_parameter(kernel_type = RBF)
+        if _svm is None:
+            raise RunTimeError('SVM Library not found. Cannot use this classifier.')
+        self.param = _svm.svm_parameter(kernel_type = RBF)
     
     def set_option(self,optname,value):
         setattr(self.param,optname,value)
 
     def _dotrain(self,features,labels):
-        problem=svm_problem(labels,features)
-        self.model=svm_model(problem,self.param)
+        problem=_svm.svm_problem(labels,features)
+        self.model=_svm.svm_model(problem,self.param)
 
     def _doapply(self,feats):
         return self.model.predict(feats)
