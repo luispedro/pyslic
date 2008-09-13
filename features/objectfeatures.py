78d932d313add8d40db257382b1dcaf0468279ee
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-09-13 21:33:22 +0000

    Major speedup by cropping the objects down to size
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@316 7947ee52-e672-4f94-911f-8123aba2f436

--- features/objectfeatures.py
+++ features/objectfeatures.py
@@ -2,6 +2,8 @@ from __future__ import division
 from ..image import Image
 from numpy import *
 from ..imageprocessing.bweuler import bweuler
+from ..imageprocessing.bbox import bbox
+from scipy import ndimage
 from scipy.ndimage import *
 from .hullfeatures import hullfeatures
 from .mmthin import mmthin
@@ -30,7 +32,7 @@ def objectfeatures(protimg,dnaimg=None,objects=None):
             warnings.warn('Bizarre combination of arguments for objectfeatures: both an Image and a separate DNA channel')
         protimg = protimg.channeldata[Image.procprotein_channel]
 
-    labeled,N = label(protimg,ones((3,3)))
+    labeled,N = ndimage.label(protimg,ones((3,3)))
     if objects is None:
         objects = xrange(1,N+1)
     if type(objects) is int:
@@ -42,25 +44,30 @@ def objectfeatures(protimg,dnaimg=None,objects=None):
         bindna=(dnaimg > 0)
     for obji,obj in enumerate(objects):
         binobj = (labeled == obj)
+        min1,max1,min2,max2=bbox(binobj)
+        if min1 > 0: min1 -= 1
+        if min2 > 0: min2 -= 1
+        binobjc = binobj[min1:max1+2,min2:max2+2]
+        protobj = protimg[min1:max1+2,min2:max2+2]
         objimg = protimg * binobj
         cofy,cofx = center_of_mass(objimg)
-        objskel = mmthin(binobj);
+        objskel = mmthin(binobjc)
         binskel = (objskel > 0)
-        objhull=convexhull(binobj)
+        objhull=convexhull(binobjc)
         no_of_branch_points = find_branch_points(objskel).sum()
-        hfeats=hullfeatures(objimg,objhull)
+        hfeats=hullfeatures(binobjc,objhull)
 
-        sofs[obji,0] = binobj.sum()
+        sofs[obji,0] = binobjc.sum()
         if dnaimg is not None:
             sofs[obji,1] = sqrt((cofy-dnacofy)**2+(cofx-dnacofx)**2)
             sofs[obji,2] = (binobj*bindna).sum()/sofs[obji,0]
         sofs[obji,3] = hfeats[2]
-        sofs[obji,4] = bweuler(binobj)
+        sofs[obji,4] = bweuler(binobjc)
         sofs[obji,5] = hfeats[1]
         sofs[obji,6] = binskel.sum()
         sofs[obji,7] = hfeats[0]
         sofs[obji,8] = sofs[obji,6]/sofs[obji,0]
-        sofs[obji,9] = (binobj*protimg).sum()/(binskel*protimg).sum()
+        sofs[obji,9] = (binobj*protimg).sum()/(binskel*protobj).sum()
         sofs[obji,10] = no_of_branch_points/sofs[obji,6]
     return sofs
 
