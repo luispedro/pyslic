39dd48702ba032a66e6299369d8dcf2774e6d663
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-09-24 15:32:45 +0000

    Fix paths
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@332 7947ee52-e672-4f94-911f-8123aba2f436

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 import numpy.distutils.core as numpyutils
 
-convexhull = numpyutils.Extension('imageprocessing/_convexhull', sources = ['imageprocessing/convexhull.cpp'])
+convexhull = numpyutils.Extension('pyslic/imageprocessing/_convexhull', sources = ['pyslic/imageprocessing/convexhull.cpp'])
 
 magick_libraries= ['Magick++','Wand','Magick'] # This is gotten from magick++-config --libs
-readimg = numpyutils.Extension('image.io.readimg', sources = ['image/io/readimg.cpp'], libraries=magick_libraries) 
+readimg = numpyutils.Extension('pyslic.image.io.readimg', sources = ['pyslic/image/io/readimg.cpp'], libraries=magick_libraries) 
+packages=setuptools.find_packages()
+packages.remove('tests')
 
 numpyutils.setup(name='PySLIC',
       version='0.3.1',
@@ -12,7 +14,7 @@ numpyutils.setup(name='PySLIC',
       author='Murphy Lab',
       author_email='murphy@mcu.edu',
       url='http://murphylab.cbi.cmu.edu/',
-      packages=setuptools.find_packages(),
+      packages=packages,
       ext_modules = [convexhull,readimg],
       test_suite='nose.collector',
       )
