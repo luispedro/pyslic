97988c4ddedd0bd73cf9655b0a7ffda561d3e7d9
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-09-25 16:43:33 +0000

    Make some tests as slow
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@343 7947ee52-e672-4f94-911f-8123aba2f436

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -4,10 +4,16 @@ L=numpy.r_[[0]*40,[1]*60]
 D=numpy.random.rand(100,10)
 D[:40] += numpy.random.rand(40,10)**2
 
+def slow(f):
+    f.slow=True
+    return f
+
+@slow
 def test_nfoldcrossvalidation():
     cmat,Ln = pyslic.classify.nfoldcrossvalidation(D,L)
     assert cmat.shape == (2,2)
 
+@slow
 def test_stringlabels():
     labelnames=['one','two']
     new_L = map(labelnames.__getitem__,L)
