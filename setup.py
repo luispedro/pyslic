ceed400eac6a67bc33aaa65219192a0439468866
luispedro <luispedro@7947ee52-e672-4f94-911f-8123aba2f436>
2008-11-05 16:46:04 +0000

    up the version number
    git-svn-id: file:///home/luispedro/svnroot/pyslic/trunk@379 7947ee52-e672-4f94-911f-8123aba2f436

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def test_pyversion():
 test_pyversion()
 
 numpyutils.setup(name='PySLIC',
-      version='0.4.3',
+      version='0.4.4',
       description='Subcellular Location Image Classifier',
       author='Murphy Lab',
       author_email='murphy@mcu.edu',
