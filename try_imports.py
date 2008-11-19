6afa187bb8df9bf5fddd6b04aa8f7bee5119b1a8
Luís Pedro Coelho <lpc@cmu.edu>
2008-11-19 15:09:10 -0500

    Check for more packages

--- try_imports.py
+++ try_imports.py
@@ -7,8 +7,6 @@ def try_imports():
     Please install numpy.
     Under linux, the package is often called python-numpy.'''
 
-    
-    
     try:
         import scipy
     except:
@@ -16,8 +14,7 @@ def try_imports():
 
     Please install scipy.
     Under linux, the package is often called python-scipy.'''
-    
-    
+
     try:
         import Image, ImageDraw
     except:
@@ -35,8 +32,6 @@ def try_imports():
     Please install scipy.weave.
 
     This is not strictly necessary, but it will make feature calculation faster (by 2~3 orders of magnitude).'''
-
-    
     
     try:
         scipy.weave.inline('')
@@ -50,7 +45,14 @@ def try_imports():
     except:
         print '''import ncreduce failed.
 
-    Ncreduce is needed for use of classification only.'''
+    Ncreduce is not necessary, but makes classification and clustering faster.'''
+
+    try:
+        import svm
+    except:
+        print '''import svm failed.
+
+    LibSVM classification will not be available (everything else will work).'''
 
 if __name__ == '__main__':
     test_imports()
