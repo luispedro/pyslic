2b38e0c5d09a366f325eb6e3e42c1a13520a2298
Luís Pedro Coelho <lpc@cmu.edu>
2009-01-26 21:36:15 -0500

    Fix greedy. Make it faster too
    When result consists of separated nuclei, we can get an empty
    values dictionary

--- pyslic/segmentation/roysam.py
+++ pyslic/segmentation/roysam.py
@@ -54,20 +54,20 @@ def roysam_watershed(dna,thresh=None,blur_factor=3):
      Vol. 56A, No. 1, pp. 23-36 Cytometry Part A, November 2003.
     '''
     if thresh is None:
-        thresh=thresholding.murphy_rc(dna)
-    M=(ndimage.gaussian_filter(dna,4)>thresh)
-    G=pymorph.gradm(dna)
-    D=ndimage.distance_transform_edt(M)
-    D=D*np.exp(1-G/float(G.max()))
-    T=ndimage.gaussian_filter(D.max() - D,blur_factor)
-    T=pymorph.to_uint8(T)
-    R=pymorph.regmin(T)
-    R,N=ndimage.label(R)
+        thresh = thresholding.murphy_rc(dna)
+    M = (ndimage.gaussian_filter(dna,4)>thresh)
+    G = pymorph.gradm(dna)
+    D = ndimage.distance_transform_edt(M)
+    D = D*np.exp(1-G/float(G.max()))
+    T = ndimage.gaussian_filter(D.max() - D,blur_factor)
+    T = pymorph.to_uint8(T)
+    R = pymorph.regmin(T)
+    R,N = ndimage.label(R)
     for i in xrange(R.size):
         if R.flat[i] == 0 and M.flat[i] == 0:
-            R.flat[i]=N+1
+            R.flat[i] = N+1
             break
-    W,WL=morph.cwatershed(T*M,R,return_lines=True)
+    W,WL = morph.cwatershed(T*M,R,return_lines=True)
     return W,WL
 
 def border(W,Bc=None):
@@ -130,7 +130,7 @@ class Merger(object):
 
     def _merge(self,c0,c1):
         '''Merge region c0 & c1'''
-        self.W[self.W==c1]=c0
+        self.W[self.W==c1] = c0
         self.neighbours[c0].remove(c1)
         self.neighbours[c1].remove(c0)
         self.neighbours[c0].update(self.neighbours[c1])
@@ -139,18 +139,19 @@ class Merger(object):
             if c1 in v:
                 v.remove(c1)
                 v.add(c0)
-        b=self.border_id[min(c0,c1),max(c0,c1)]
+        b = self.border_id[min(c0,c1),max(c0,c1)]
         del self.border_id[min(c0,c1),max(c0,c1)]
-        self.B[self.B==b]=0
+        self.B[self.B==b] = 0
         for (r0,r1),b in self.border_id.items():
             if r0 == c1 or r1 == c1:
                 del self.border_id[r0,r1]
-                if r1 == c1: r1=r0
-                d0,d1=min(r1,c0),max(r1,c0)
+                if r1 == c1:
+                    r1 = r0
+                d0,d1 = min(r1,c0),max(r1,c0)
                 if (d0,d1) in self.border_id:
                     self.B[self.B==b] = self.border_id[d0,d1]
                 else:
-                    self.border_id[d0,d1]=b
+                    self.border_id[d0,d1] = b
                 
     def _Rw(self,c0,c1):
         '''Implement Rw in the paper.'''
@@ -174,17 +175,27 @@ class Merger(object):
         '''
         Merge regions greedily.
         '''
-        border_regions=dict((y,x) for x,y in self.border_id.iteritems())
+        values = dict(((c0,c1),self._Rw(c0,c1)) for (c0,c1),b in self.border_id.iteritems())
         while True:
-            values=dict((b,self._Rw(c0,c1)) for (c0,c1),b in self.border_id.iteritems())
-            val,best=max((y,x) for x,y in values.items())
+            if not values:
+                break
+            val,best = max((y,x) for x,y in values.items())
             if val < beta: break
-            self._merge(*border_regions[best])
-            border_regions=dict((y,x) for x,y in self.border_id.iteritems())
+            b0,b1 = best
+            affected = self.neighbours[b0]|self.neighbours[b1]
+            recompute = []
+            for c0,c1 in values.keys():
+                if c0 in affected or c1 in affected:
+                    del values[c0,c1]
+                    recompute.append((c0,c1))
+            self._merge(b0,b1)
+            for c0,c1 in self.border_id:
+                if (c0,c1) not in values:
+                    values[c0,c1] = self._Rw(c0,c1)
         return self.W
 
 def greedy_roysam_merge(dna,mu,iSigma):
-    M=Merger(dna,mu,iSigma)
+    M = Merger(dna,mu,iSigma)
     M.greedy()
     return M.W
 
