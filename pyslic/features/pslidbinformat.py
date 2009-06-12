a0b3de681c44d3f0c73f145f5a027c939a8c2ed3
Luís Pedro Coelho <lpc@cmu.edu>
2009-06-12 02:20:03 -0400

    Implement PSLID's binary format (version 5).
    This is mostly from the webpage, but with the Matlab code taking
    precedence when it differed from the docs.

---
+++ pyslic/features/pslidbinformat.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009  Murphy Lab
+# Carnegie Mellon University
+#
+# Written by Luís Pedro Coelho <lpc@cmu.edu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# For additional information visit http://murphylab.web.cmu.edu or
+# send email to murphy@cmu.edu
+
+from __future__ import division, with_statement
+from struct import pack, unpack
+
+__all__ = ['readpslidbin','writepslidbin']
+def readpslidbin(filename):
+    input = file(filename)
+    def read_int32():
+        bytes = input.read(4)
+        return unpack('>i',bytes)[0]
+    def read_float32():
+        bytes = input.read(4)
+        return unpack('>f',bytes)[0]
+    def read_str():
+        nbytes = read_int32()
+        return input.read(nbytes)
+    def read_strlist():
+        return read_str().split('@')
+    def read_intlist():
+        n = read_int32()
+        return [read_int32() for i in xrange(n)]
+    version = read_int32()
+    if version != 5:
+        raise NotImplementedError('pyslic.readpslidbin: Can only read version 5 files')
+    rows = read_int32()
+    cols = read_int32()
+    features = [read_float32() for i in xrange(cols)]
+    featids = read_intlist()
+    real_slf_names = read_strlist()
+    slf_names = read_strlist()
+    names = read_str().split('@')
+    n_sampleids = read_int32()
+    samples = [read_int32() for i in xrange(n_sampleids)]
+    settype = read_int32()
+    imageurls = read_str().split('@')
+    maskurls = read_str().split('@')
+    #ndims = read_int32()  # This is in the documentation but not in the matlab code
+    n_channels = read_int32()
+    channel_nrs = [read_int32() for i in xrange(n_channels)]
+        
+def writepslidbin(output, features, real_slf_names, slf_names, names, imageurls, maskurls, settype, channel_nrs):
+    if type(output) in (str,unicode):
+        output = file(output, 'w')
+    def write_int32(x):
+        output.write(pack('>i',x))
+    def write_float32(x):
+        output.write(pack('>f',x))
+    def write_strlist(s):
+        s = ''.join(a+'@' for a in s)
+        output.write(s)
+    def write_intlist(l):
+        write_int32(len(l))
+        for i in l:
+            write_int32(i)
+    rows,cols = features.shape
+    write_int32(5) # Version
+    write_int32(rows)
+    write_int32(cols)
+    for fs in features:
+        for f in fs:
+            write_float32(f)
+    write_strlist(real_slf_names)
+    write_strlist(slf_names)
+    write_strlist(names)
+    write_intlist([])
+    write_int32(settype)
+    write_strlist(imageurls)
+    write_strlist(maskurls)
+    write_intlist(channel_nrs)
