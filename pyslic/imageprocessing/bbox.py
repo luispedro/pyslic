42318a1d69d6b93413923c860214c8a14044fbb6
Luis Pedro Coelho <lpc@cmu.edu>
2010-05-10 16:16:55 -0400

    croptobbox, bbox now in mahotas

--- pyslic/imageprocessing/bbox.py
+++ pyslic/imageprocessing/bbox.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008  Murphy Lab
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # Carnegie Mellon University
 # 
 # Written by Luis Pedro Coelho <lpc@cmu.edu>
@@ -22,72 +23,9 @@
 # For additional information visit http://murphylab.web.cmu.edu or
 # send email to murphy@cmu.edu
 
-from numpy import *
+from mahotas import bbox, croptobbox
+import numpy as np
 __all__ = ['bbox', 'croptobbox', 'expandto']
-def bbox(img):
-    """
-    min1,max1,min2,max2 = bbox(img)
-
-    Calculate the bounding box of image img.
-    """
-    min1=0
-    min2=0
-    max1,max2=img.shape
-    try:
-        from scipy import weave
-        from scipy.weave import converters
-        vals=array([max1,max2,min1,min2])
-        img=asarray(img,uint8)
-        code='''
-        int max1=vals(0);
-        int max2=vals(1);
-        for (int y = 0; y != max1; ++y) {
-            for (int x = 0; x != max2; ++x) {
-                if (img(y,x) > 0) {
-                    if (y < vals(0)) vals(0) = y;
-                    if (x < vals(1)) vals(1) = x;
-                    if (y >= vals(2)) vals(2) = y+1;
-                    if (x >= vals(3)) vals(3) = x+1;
-                }
-            }
-        }
-        '''
-        weave.inline(
-                code,
-                ['vals','img'],
-                type_converters=converters.blitz)
-        min1,min2,max1,max2=tuple(vals)
-        if min1 > max1: # special case: image is all zeros
-            return 0,0,0,0
-    except:
-        pos1,=where(img.any(1))
-        if len(pos1) == 0:
-            return 0,0,0,0
-        min1=pos1[0]
-        max1=pos1[-1]+1
-
-        pos2,=where(img.any(0))
-        min2=pos2[0]
-        max2=pos2[-1]+1
-    return min1,max1,min2,max2
-
-def croptobbox(img, border=None):
-    """
-    Returns a version of img cropped to the image's bounding box
-
-    Parameters
-    ----------
-        * img: image
-        * border: whether to add a border (default no border)
-    """
-    
-    min1,max1,min2,max2 = bbox(img)
-    if border is not None:
-        min1 = max(0, min1-border)
-        min2 = max(0, min2-border)
-        max1 += border
-        max2 += border
-    return img[min1:max1,min2:max2]
 
 def expandto(img,S1,S2):
     """
@@ -97,7 +35,7 @@ def expandto(img,S1,S2):
     Img will be on the top left of the result.
     """
 
-    result=zeros((S1,S2),img.dtype)
+    result = np.zeros((S1,S2),img.dtype)
     M,N=img.shape
     assert M <= S1
     assert N <= S2
@@ -106,4 +44,3 @@ def expandto(img,S1,S2):
     result[st1:(st1+M),st2:(st2+N)]=img
     return result
 
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
