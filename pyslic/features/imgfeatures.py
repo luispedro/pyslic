01bf239b512c3cb8deec9b83ff67cb1c62fd5269
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-23 18:11:09 -0400

    Remove one more dependency on weave
    Moved things to mahotas too.

--- pyslic/features/imgfeatures.py
+++ pyslic/features/imgfeatures.py
@@ -5,7 +5,7 @@
 # Jun 2, 2002 - M.Velliste: added SLF names
 # Feb 7 2008 - L-P Coelho: Ported to Python
 
-# Copyright (C) 2006-2009 Murphy Lab
+# Copyright (C) 2006-2010 Murphy Lab
 # Carnegie Mellon University
 #
 # This program is free software; you can redistribute it and/or modify
@@ -41,10 +41,10 @@ def imgfeatures(imageproc, isfield):
     values = imgfeatures(imageproc, isfield=False)
 
     Calculates object features for imageproc
-    where imageproc contains the pre-processed fluorescence image, 
+    where imageproc contains the pre-processed fluorescence image,
     Pre-processed means that the image has been cropped and had 
     pixels of interest selected (via a threshold, for instance).
- 
+
     Features calculated include:
       - Number of objects
       - Euler number of the image (# of objects - # of holes)
@@ -65,12 +65,12 @@ def imgfeaturesdna(imageproc, dnaproc, isfield=False):
     values = imgfeaturesdna(imageproc, dnaproc, isfield=False)
 
     calculates object features for imageproc
-    where imageproc contains the pre-processed fluorescence image, 
+    where imageproc contains the pre-processed fluorescence image,
     and dnaproc the pre-processed dna fluorescence image.
-    Pre-processed means that the image has been cropped and had 
+    Pre-processed means that the image has been cropped and had
     pixels of interest selected (via a threshold, for instance).
-    use dnaproc=None to exclude features based on the dna image.  
-    
+    use dnaproc=None to exclude features based on the dna image.
+
     Parameters
     ----------
         * imageproc: pre-processed image
@@ -92,97 +92,52 @@ def imgfeaturesdna(imageproc, dnaproc, isfield=False):
       - DNA: ratio of the largest object distance to the smallest
       - DNA/Image: distance of the DNA COF to the image COF [unless isfield]
       - DNA/Image: ratio of the DNA image area to the image area
-      - DNA/Image: fraction of image that overlaps with DNA 
+      - DNA/Image: fraction of image that overlaps with DNA
     """
     bwimage = (imageproc > 0)
-    imagelabeled,obj_number = ndimage.label(bwimage)
-    values = [obj_number]
-    dnacof = None
-
-    if obj_number == 0:
+    imagelabeled,nr_objs = ndimage.label(bwimage)
+    if not nr_objs:
         if isfield:
             if dnaproc is None: return np.zeros(5)
             raise NotImplementedError
         if dnaproc is None: return np.zeros(8)
         return np.zeros(14)
 
+    values = [nr_objs]
+    dnacof = None
     euler_nr = euler(bwimage)
     values.append(euler_nr)
 
     cof = None
     if not isfield:
-        cof = np.array(ndimage.center_of_mass(imageproc))
+        cof = mahotas.center_of_mass(imageproc)
 
-    # Find the maximum and minimum object sizes, and the distance 
-    #    of each object to the center of fluorescence
-    obj_distances = []
-    obj_dnadistances = []
+    obj_sizes = np.array([(imagelabeled == (oi+1)).sum() for oi in xrange(nr_objs)])
 
-    moment_length = obj_number  + 1
-    img_moment00 = np.zeros(moment_length);
-    img_moment10 = np.zeros(moment_length);
-    img_moment01 = np.zeros(moment_length);
-    obj_sizes = np.zeros(moment_length);
-    
-    r,c=imageproc.shape
-    try:
-        from scipy import weave
-        from scipy.weave import converters
-        code='''
-        for (int x = 0; x != c; ++x) {
-            for (int y = 0; y != r; ++y) {
-                unsigned moment_array_index = imagelabeled(y,x);
-                img_moment00(moment_array_index) += imageproc(y,x);
-                img_moment10(moment_array_index) += (x * imageproc(y,x));
-                img_moment01(moment_array_index) += (y * imageproc(y,x));
-                ++obj_sizes(moment_array_index);
-            }
-        }
-        '''
-        weave.inline(code,
-            ['r','c','imageproc','imagelabeled','obj_sizes','img_moment00','img_moment01','img_moment10'],
-            type_converters=converters.blitz)
-    except ImportError:
-        img_moment00 = np.zeros(moment_length);
-        img_moment10 = np.zeros(moment_length);
-        img_moment01 = np.zeros(moment_length);
-        obj_sizes = np.zeros(moment_length);
-        for x in xrange(c):
-            for y in xrange(r):
-                moment_array_index = imagelabeled[y,x]
-                img_moment00[moment_array_index] += imageproc[y,x];
-                img_moment10[moment_array_index] += (x * imageproc[y,x]);
-                img_moment01[moment_array_index] += (y * imageproc[y,x]);
-                obj_sizes[moment_array_index] += 1
-
-    obj_sizes = np.array(obj_sizes[1:]) # Ignore object 0, i.e. background
     obj_size_avg = obj_sizes.mean()
     obj_size_var = obj_sizes.var()
-    obj_size_ratio = obj_sizes.max()/obj_sizes.min() 
+    obj_size_ratio = obj_sizes.max()/obj_sizes.min()
 
     values.append(obj_size_avg)
-    values.append(obj_size_var) 
+    values.append(obj_size_var)
     values.append(obj_size_ratio)
 
 
     if not isfield:
+        obj_cms = mahotas.center_of_mass(imageproc, imagelabeled)
+        obj_distances = []
+        obj_dnadistances = []
         if dnaproc is not None:
             dnacof = np.array(ndimage.center_of_mass(dnaproc))
-        for i in xrange(1,obj_number + 1):
-            obj_m00 = float(img_moment00[i]);
-            obj_m10 = float(img_moment10[i]);
-            obj_m01 = float(img_moment01[i]);
-
-            obj_center = np.array([obj_m01,obj_m10],float)/obj_m00;
+        for obj_center in obj_cms:
             obj_distance = _norm2(obj_center - cof)
-            
             obj_distances.append(obj_distance)
 
             if dnaproc is not None:
                 obj_dnadistance = _norm2(obj_center - dnacof)
                 obj_dnadistances.append(obj_dnadistance)
         obj_distances = np.array(obj_distances)
-        obj_dist_avg = obj_distances.mean() ;
+        obj_dist_avg = obj_distances.mean()
         obj_dist_var = obj_distances.var()
         mindist = obj_distances.min()
         if mindist != 0:
@@ -200,7 +155,7 @@ def imgfeaturesdna(imageproc, dnaproc, isfield=False):
             obj_dnadist_var = obj_dnadistances.var()
             obj_dnamindist=obj_dnadistances.min()
             if obj_dnamindist != 0:
-                obj_dnadist_ratio = obj_dnadistances.max()/obj_dnamindist 
+                obj_dnadist_ratio = obj_dnadistances.max()/obj_dnamindist
             else:
                 obj_dnadist_ratio = 0 ;
             values.append(obj_dnadist_avg)
@@ -212,7 +167,7 @@ def imgfeaturesdna(imageproc, dnaproc, isfield=False):
     if dnaproc is not None:
         dna_area = (dnaproc > 0).sum()
         image_area = (imageproc > 0).sum()
-        
+
         # what fraction of the image fluorescence area overlaps the dna image?
         image_overlap = ( (imageproc > 0) & (dnaproc > 0) ).sum()
 
@@ -220,9 +175,9 @@ def imgfeaturesdna(imageproc, dnaproc, isfield=False):
             dna_image_area_ratio = 0
             image_dna_overlap = 0
         else:
-            dna_image_area_ratio = dna_area/image_area ;
-            image_dna_overlap = image_overlap/image_area ;
-        
+            dna_image_area_ratio = dna_area/image_area
+            image_dna_overlap = image_overlap/image_area
+
         if dnacof is not None:
             dna_image_distance = _norm2(cof-dnacof)
             values.append(dna_image_distance)
