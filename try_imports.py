93124b9bbfafc0902525893667ae6127c0a6a48e
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-24 00:51:44 -0400

    Less dependencies now

--- try_imports.py
+++ try_imports.py
@@ -16,35 +16,6 @@ def try_imports():
     Under linux, the package is often called python-scipy.'''
 
     try:
-        import readmagick
-    except ImportError:
-        try:
-            import Image, ImageDraw
-        except ImportError:
-
-            print '''Neither readmagick nor the Python Imaging Library (PIL) are present!
-
-        Please install readmagick or PIL.'''
-
-
-
-    try:
-        import scipy.weave
-    except:
-        print '''Scipy.weave not present!
-
-    Please install scipy.weave.
-
-    This is not strictly necessary, but it will make feature calculation faster (by 2~3 orders of magnitude).'''
-    
-    try:
-        scipy.weave.inline('')
-    except:
-        print '''Scipy.weave.inline() failed.
-
-    This is not strictly necessary, but it will make feature calculation faster (by 2~3 orders of magnitude).'''
-
-    try:
         import ncreduce
     except:
         print '''import ncreduce failed.
