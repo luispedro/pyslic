24f10a4b5d9ea9100864a05a9afe800be3194aa8
Luis Pedro Coelho <lpc@cmu.edu>
2010-08-30 17:17:11 -0400

    Remove tests for removed functions

--- tests/test_imageprocessing.py
+++ tests/test_imageprocessing.py
@@ -2,23 +2,9 @@ import numpy
 import numpy as np
 import pyslic
 import pyslic.imageprocessing.thresholding
-from pyslic.imageprocessing.basics import fullhistogram
 from os.path import dirname
 img = pyslic.readimg(dirname(__file__) + '/data/protimg.jp2')
 
-def test_fullhistogram():
-    A100 = numpy.arange(100).reshape((10,10))
-    assert fullhistogram(A100).shape == (100,)
-    assert numpy.all(fullhistogram(A100) == numpy.ones(100))
-
-    A1s = numpy.ones((12,12))
-    assert fullhistogram(A1s).shape == (2,)
-    assert numpy.all(fullhistogram(A1s) == numpy.array([0,144]))
-
-    A1s[0] = 0
-    A1s[1] = 2
-    assert fullhistogram(A1s).shape == (3,)
-    assert numpy.all(fullhistogram(A1s) == numpy.array([12,120,12]))
 
 def test_threshold():
     assert pyslic.imageprocessing.thresholding.threshold(img,None)  == -1
