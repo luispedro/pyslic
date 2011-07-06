8d2076bd4409a86f8d95676e3e1c5d1035522df5
Luis Pedro Coelho <lpc@cmu.edu>
2011-07-06 18:53:21 -0400

    ENH Correctly set .label & .id

--- pyslic/image/io/tiff_pairs.py
+++ pyslic/image/io/tiff_pairs.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2010  Murphy Lab, Carnegie Mellon University
+# Copyright (C) 2010-2011  Murphy Lab, Carnegie Mellon University
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 #
 # Written by Luis Pedro Coelho <lpc@cmu.edu>
@@ -24,7 +24,15 @@
 
 from __future__ import division, with_statement
 from os import listdir, path
+import re
+from collections import defaultdict
 import pyslic
+
+_name_pat = re.compile('([A-Z0-9]{5})_Image_([0-9]+)_(prot|dna).tiff')
+def _channelname(c):
+    if c == 'prot':
+        return 'protein'
+    return c
 
 def read_tiff_pairs_dir(directory):
     '''
@@ -34,24 +42,16 @@ def read_tiff_pairs_dir(directory):
         (i.e., image_prot.tiff/image_dna.tiff)
     and returns a list of the images found.
     '''
-    pairs = {}
-    for img in listdir(directory):
-        if img.endswith('_prot.tiff'):
-            pairs[img[:-len('_prot.tiff')], 'protein'] = img
-        elif img.endswith('_dna.tiff'):
-            pairs[img[:-len('_dna.tiff')], 'dna'] = img
-
-    imgs = []
-    for label,channel in pairs:
-        if channel != 'protein': continue
-        if (label, 'dna') not in pairs:
-            raise IOError("Unmatched protein pair: %s" % label)
-        img = pyslic.Image()
-        img.channels['dna'] = path.join(directory, pairs[label, 'dna'])
-        img.channels['protein'] = path.join(directory, pairs[label, 'protein'])
-        img.label = label
-        imgs.append(img)
-    return imgs
+    imgs = defaultdict(pyslic.image.Image)
+    for fname in listdir(directory):
+        match = _name_pat.match(fname)
+        if match is not None:
+            code,nr,type = match.groups()
+            img = imgs[code,nr]
+            img.channels[_channelname(type)] = path.abspath(path.join(directory,fname))
+            img.label = code[3:]
+            img.id = (img.label, nr)
+    return imgs.values()
 
 
 def detect_tiff_pairs(directory):
